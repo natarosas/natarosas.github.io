b0252332e440ceea5914722d87c973bf40af8733
NATALIA ROSAS <nataliarosas1010@gmail.com>
2016-09-15 22:21:35 -0500

    Change the background, add a new file for css and add JavaScript

---
+++ js/helper.js
@@ -0,0 +1,6 @@
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
---
+++ js/resumeBuilder.js
@@ -0,0 +1,41 @@
+var work = { 
+	"jobs": [
+      {
+        "employer":  "Mensajeros Urbanos",
+        "title": "Operations Manager",
+        "location": "Bogotá, Colombia",
+        "dates": "Oct/2015 - Jan/2016",
+        "description": "Monitoring each messaging service to verify its effectiveness and quality." + "<br>" + 
+        "Management and coordination of motorized staff." + "<br>" + "Customer service on telephone at night shift." + "<br>" + 
+        "Scheduling and assignment of delivery routes."
+      },
+	  {
+	  	"employer":  "University of los Andes",
+		"title": "Tech Asistant",
+		"location": "Bogotá, Colombia",
+		"dates": "Ago/2015 - Oct/2015",
+		"description": "Advice on technical processes in computer rooms." + "<br>" + "Attention and supervision of students in computer rooms."
+	  }
+	]
+}
+
+work.display = function()  {
+
+	for (job in work.jobs){
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    
+    	$(".work-entry:last").append(formattedEmployerTitle);
+    	$(".work-entry:last").append(formattedLocation);
+    	$(".work-entry:last").append(formattedDates);
+    	$(".work-entry:last").append(formattedDescription);
+	}
+}
+
+work.display();
